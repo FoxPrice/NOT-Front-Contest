6694fb92dd19e5a955dc4760ecf0eb3e904e918a
Fox <chloefoxlisa@gmail.com>
2025-06-09 17:01:08 +0400

    Husky fixed

--- src/store/provider.jsx
+++ src/store/provider.jsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { Provider } from "react-redux";
-import { store } from "./index";
+import { Provider } from 'react-redux';
+import { store } from './index';
 
 export function ReduxProvider({ children }) {
-    return <Provider store={store}>{children}</Provider>
+    return <Provider store={store}>{children}</Provider>;
 }
